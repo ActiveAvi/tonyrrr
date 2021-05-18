91733ba1023be61ef39ceccaaabf8939f69c1462
ActiveAvi <loch.hollows@gmail.com>
2021-05-18 14:45:47 -0400

    added img opt; added func to get mdx posts data;

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,52 @@
 import Link from 'next/link'
+import styles from '../theme/index.module.css'
+import { getPostsMetaData } from '../lib/getPostsData'
 
-export default function Index() {
+export default function Home({ postsData }) {
   return (
-    <div>
+    <div className={styles.infoContainer}>
       <h1>Index</h1>
       <Link href='/about'>
         <a>About</a>
       </Link>
+      <picture>
+        <source
+          srcSet={require('../images/gates-epstein.jpg?webp')}
+          type='image/webp'
+        />
+        <source
+          srcSet={require('../images/gates-epstein.jpg')}
+          type='image/jpg'
+        />
+        <img
+          className={styles.infoImg}
+          src={require('../images/gates-epstein.jpg')}
+          alt='Gates and Epstein chilling at Epsteins mansion'
+        />
+      </picture>
+      <p className={styles.infoDescription}>
+        Epstein and Gates were hanging out, why?
+      </p>
+      <hr />
+      {postsData.map((metadata) => {
+        return (
+          <div key={metadata.id}>
+            <Link href={`/blog/${metadata.id}`} key={metadata.title}>
+              <a className={styles.postTitle}> {metadata.title} </a>
+            </Link>
+            <p className={styles.postDescription}> {metadata.description} </p>
+          </div>
+        )
+      })}
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const postsData = getPostsMetaData()
+  return {
+    props: {
+      postsData: postsData,
+    },
+  }
 }
