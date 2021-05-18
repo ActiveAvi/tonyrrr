a22df8429ee7473efdb4e521b2082ccf4743f37b
ActiveAvi <loch.hollows@gmail.com>
2021-05-18 14:46:27 -0400

    fetches mdx posts, creates static routes and pages

---
+++ pages/blog/[id].js
@@ -0,0 +1,66 @@
+import { serialize } from 'next-mdx-remote/serialize'
+import { MDXRemote } from 'next-mdx-remote'
+import { getAllPostsPath, getPostData } from '../../lib/getPostsData.js'
+
+const components = {
+  h1: (props) => (
+    <h1
+      style={{
+        fontSize: 'calc(1rem + 1.5vw)',
+        color: 'black',
+        margin: '1vh 0 1vh 0',
+      }}
+      {...props}
+    />
+  ),
+
+  p: (props) => (
+    <p
+      style={{
+        fontSize: 'calc(1rem + 0.1vw)',
+        color: '#000000e6',
+        margin: '0vh 0 1vh 0',
+      }}
+      {...props}
+    />
+  ),
+}
+
+export default function Blog({ postMetadata, postContent }) {
+  return (
+    <div>
+      <MDXRemote {...postContent} components={components} />
+
+      <style jsx>{`
+        .blog-content {
+          display: flex;
+          flex: 100%;
+          flex-direction: column;
+          margin: 1vw 25vw 1vw 25vw;
+          width: 50vw;
+          max-width: 50vw;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostsPath()
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  const mdxSource = await serialize(postData.content, { components })
+  return {
+    props: {
+      postMetadata: postData.metadata,
+      postContent: mdxSource,
+      id: params.id,
+    },
+  }
+}
