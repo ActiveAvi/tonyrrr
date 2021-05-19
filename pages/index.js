d916e7060f2a0be0e6436425395559994eed4275
ActiveAvi <loch.hollows@gmail.com>
2021-05-18 22:59:10 -0400

    removed blog posts; added jumbotron for hero-img

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,23 @@
-import Link from 'next/link'
 import styles from '../theme/index.module.css'
-import { getPostsMetaData } from '../lib/getPostsData'
+import { Jumbotron, Button, Container } from 'react-bootstrap'
 
-export default function Home({ postsData }) {
+export default function Home() {
   return (
-    <div className={styles.infoContainer}>
-      <h1>Welcome aboard fellow freedom-seekers!</h1>
-      <picture>
-        <source
-          srcSet={require('../images/gates-epstein.jpg?webp')}
-          type='image/webp'
-        />
-        <source
-          srcSet={require('../images/gates-epstein.jpg')}
-          type='image/jpg'
-        />
-        <img
-          className={styles.infoImg}
-          src={require('../images/gates-epstein.jpg')}
-          alt='Gates and Epstein chilling at Epsteins mansion'
-        />
-      </picture>
-      <p className={styles.infoDescription}>
-        Epstein and Gates were hanging out, why?
-      </p>
-      <hr />
-      {postsData.map((metadata) => {
-        return (
-          <div key={metadata.id}>
-            <Link href={`/blog/${metadata.id}`} key={metadata.title}>
-              <a className={styles.postTitle}> {metadata.title} </a>
-            </Link>
-            <p className={styles.postDescription}> {metadata.description} </p>
-          </div>
-        )
-      })}
+    <div className={styles.pageContainer}>
+      <Jumbotron className={styles.jumbo}>
+        <Container>
+          <h1>
+            Welcome aboard, <br />
+            freedom-seekers!
+          </h1>
+          <p>
+            This ship belongs to TonyRrr. I'm a web developer, freedom advocate,
+            business creator, and above-all, an autonomous pirate on the nets
+            highseas.
+          </p>
+          <Button variant='primary'>Consult with me</Button>
+        </Container>
+      </Jumbotron>
     </div>
   )
-}
-
-export async function getStaticProps() {
-  const postsData = getPostsMetaData()
-  return {
-    props: {
-      postsData: postsData,
-    },
-  }
 }
