ee16c4623db219bb78ba7d32c1973f53e4c007d0
ActiveAvi <loch.hollows@gmail.com>
2021-05-18 22:58:50 -0400

    Moved blog post list to blog page

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,38 @@
-export default function Blog() {
+import { getPostsMetaData } from '../lib/getPostsData'
+import styles from '../theme/blog.module.css'
+import Link from 'next/link'
+import { Card, Button } from 'react-bootstrap'
+
+export default function Blog({ postsData }) {
   return (
-    <div>
-      <h1>Blog</h1>
+    <div className={styles.postsContainer}>
+      {postsData.map((metadata) => {
+        return (
+          <Card className={styles.postCard} key={metadata.id}>
+            <Link href={`/blog/${metadata.id}`} key={metadata.title}>
+              <Card.Title className={styles.postTitle}>
+                {metadata.title}
+              </Card.Title>
+            </Link>
+            <Card.Text>{metadata.description}</Card.Text>
+            <Button
+              href={`/blog/${metadata.id}`}
+              variant='outline-primary'
+              size='sm'>
+              Read post
+            </Button>
+          </Card>
+        )
+      })}
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const postsData = getPostsMetaData()
+  return {
+    props: {
+      postsData: postsData,
+    },
+  }
 }
