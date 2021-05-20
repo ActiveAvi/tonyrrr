e496faeb94bfbca8e5100b6ba762d518a4609c03
ActiveAvi <loch.hollows@gmail.com>
2021-05-19 23:18:47 -0400

    Started adding project cards

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import styles from '../theme/index.module.css'
-import { Jumbotron, Button, Container } from 'react-bootstrap'
+import { Jumbotron, Button, Container, Row, Col, Card } from 'react-bootstrap'
 
 export default function Home() {
   return (
@@ -15,9 +15,28 @@ export default function Home() {
             business creator, and above-all, an autonomous pirate on the nets
             highseas.
           </p>
-          <Button variant='primary'>Consult with me</Button>
+          <Button href='/contact' variant='primary'>
+            Consult with me
+          </Button>
         </Container>
       </Jumbotron>
+      <Container>
+        <Row>
+          <Col>
+            <Card bg='light' text='dark' style={{ width: '14rem' }}>
+              <Card.Header>
+                <Card.Title>TonyRrr.dev</Card.Title>
+                <Card.Subtitle className='mb-2 text-muted'>
+                  Next.js, Github, IPFS
+                </Card.Subtitle>
+              </Card.Header>
+              <Card.Body>
+                <Card.Text></Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
