2ba5fa28d6dd15167c5445e65223b51ed737affc
ActiveAvi <loch.hollows@gmail.com>
2021-05-19 23:17:51 -0400

    changed nav breakpoint; added key to menu items

--- components/navi.js
+++ components/navi.js
@@ -5,7 +5,7 @@ import styles from './navi.module.css'
 export default function Navi() {
   const menu = ['/', '/bio', '/blog', '/projects', '/contact']
   return (
-    <Navbar bg='dark' variant='dark' expand='lg'>
+    <Navbar bg='dark' variant='dark' expand='sm'>
       <Link href='/'>
         <Navbar.Brand className={styles.brand} bg='dark'>
           TonyRrr!
@@ -19,11 +19,9 @@ export default function Navi() {
         <Nav>
           {menu.map((route) => {
             return (
-              <>
-                <Nav.Link className={styles.navlink} href={route}>
-                  {route.slice(1)}
-                </Nav.Link>
-              </>
+              <Nav.Link key={route} className={styles.navlink} href={route}>
+                {route.slice(1)}
+              </Nav.Link>
             )
           })}
         </Nav>
