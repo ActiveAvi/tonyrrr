d3a0c95db4c693e840d2ba3f65c3ffde17d0d412
ActiveAvi <loch.hollows@gmail.com>
2021-05-21 11:17:35 -0400

    added smart scrolling nav bar

--- components/navi.js
+++ components/navi.js
@@ -1,11 +1,52 @@
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
 import { Navbar, Nav, Form, Button, FormControl } from 'react-bootstrap'
 import styles from './navi.module.css'
 
 export default function Navi() {
   const menu = ['/', '/bio', '/blog', '/projects', '/contact']
+  const [scrollingDown, setScrollingDown] = useState(false)
+
+  let previousScrollPosition = 0
+
+  const handleScroll = (e) => {
+    let currentScrollPosition = window.ScrollY || window.pageYOffset
+    if (currentScrollPosition > previousScrollPosition) {
+      setScrollingDown(true)
+    } else {
+      setScrollingDown(false)
+    }
+    previousScrollPosition = currentScrollPosition
+  }
+
+  // throttle for the scroll func
+  let throttleWait
+
+  const throttle = (callback, time) => {
+    if (throttleWait) return
+    throttleWait = true
+    setTimeout(() => {
+      callback()
+      throttleWait = false
+    }, time)
+  }
+
+  // set up event listener for scroll
+  // clean up listener if component unmounts
+  useEffect(() => {
+    const listener = window.addEventListener('scroll', () => {
+      throttle(handleScroll, 250)
+    })
+    return () => window.removeEventListener('scroll', listener)
+  }, [])
+
+  let navClasses = `${styles.nav} ${styles.scrollUp}`
+  if (scrollingDown) {
+    navClasses = `${styles.nav} ${styles.scrollDown}`
+  }
+
   return (
-    <Navbar bg='dark' variant='dark' expand='sm'>
+    <Navbar className={navClasses} bg='dark' variant='dark' expand='sm'>
       <Link href='/'>
         <Navbar.Brand className={styles.brand} bg='dark'>
           TonyRrr!
