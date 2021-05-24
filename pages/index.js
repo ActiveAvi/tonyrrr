3e6b4faa450ce7a8120cfeebed87b485c81c83dc
tony_rrr <tony@tonyreurink.com>
2021-05-24 18:12:20 -0400

    added Service comp to page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import styles from '../styles/index.module.css'
 import Hero from '../components/hero'
+import Services from '../components/services'
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Services />
     </div>
   )
 }
