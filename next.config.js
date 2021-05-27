e0651f2ecad878c08265880f02d0c45ec95b2bcb
ActiveAvi <loch.hollows@gmail.com>
2021-05-26 22:19:56 -0400

    Removed optimizedimages until bug is resolved

--- next.config.js
+++ next.config.js
@@ -1,30 +1,28 @@
-const withNextOptimizedImages = require('next-optimized-images')
+// const withNextOptimizedImages = require('next-optimized-images')
 
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 })
 
-module.exports = withNextOptimizedImages(
-  withMDX({
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.node = {
-          fs: 'empty',
-        }
+module.exports = withMDX({
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
       }
-      return config
-    },
-    pageExtensions: ['js', 'jsx', 'mdx'],
-    target: 'serverless',
-    trailingSlash: true,
-    exportPathMap: function () {
-      return {
-        '/': { page: '/' },
-        '/bio': { page: '/bio' },
-        '/blog': { page: '/blog' },
-        '/projects': { page: '/projects' },
-        '/contact': { page: '/contact' },
-      }
-    },
-  })
-)
+    }
+    return config
+  },
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  target: 'serverless',
+  trailingSlash: true,
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+      '/bio': { page: '/bio' },
+      '/blog': { page: '/blog' },
+      '/projects': { page: '/projects' },
+      '/contact': { page: '/contact' },
+    }
+  },
+})
