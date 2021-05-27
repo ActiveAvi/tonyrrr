b7e5287a2100cc4b01ac9f2ae8d15a086fc9c6e0
ActiveAvi <loch.hollows@gmail.com>
2021-05-26 22:21:31 -0400

    Added bio and porjects section

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Hero from '../components/hero'
 import Services from '../components/services'
+import Bio from '../components/bio'
+import Projects from '../components/projects'
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Services />
+      <Bio />
+      <Projects />
     </div>
   )
 }
