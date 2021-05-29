cfa3c3e1c83c018ce61329c3e7eb30528170fcaf
ActiveAvi <loch.hollows@gmail.com>
2021-05-29 12:53:39 -0400

    style touch-up

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,15 @@
 import Navi from './navi'
 
 const classes = {
-  main: `text-gray-300 font-mono`,
+  main: `text-gray-300 font-mono `,
+  content: 'sm:ml-1/6 bg-warmgray-800',
 }
 
 export default function Layout({ preview, children }) {
   return (
     <div className={classes.main}>
       <Navi />
-      {children}
+      <main className={classes.content}>{children}</main>
     </div>
   )
 }
