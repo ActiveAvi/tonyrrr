6146469d9c3fc60f3d11b5d5643213bdfe7053da
ActiveAvi <loch.hollows@gmail.com>
2021-05-30 19:12:32 -0400

    added auto-close of menu when page changes

--- components/navi.js
+++ components/navi.js
@@ -1,9 +1,8 @@
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-{
-  /*stylesheets and images*/
-}
+
+/*stylesheets and images*/
 import styles from '../styles/navi.module.css'
 import Helm from '../images/004-helm.svg'
 
@@ -15,20 +14,34 @@ const classes = {
   navLinks: (showLinks) =>
     `sm:block transition-all ease-in duration-300 transform mt-h-30 `,
   navLink: (showLinks) =>
-    `block lowercase msm:text-3xl text-md font-mono hover:text-red-200`,
+    `block lowercase msm:text-3xl text-md font-mono hover:text-red-200 cursor-pointer`,
   brand:
     'hidden sm:inline-block font-mono uppercase text-lg hover:text-red-400 w-max mt-4',
   navSpan: 'flex-grow',
   navButton: 'sm:hidden',
-  helm: 'z-30 h-14 w-14 fixed bottom-0 right-0 mr-2 mb-2 sm:hidden',
+  helm: 'z-30 h-14 w-14 fixed bottom-0 right-0 mr-2 mb-2 sm:hidden animate-spin-slow',
 }
 
 export default function Navi() {
   const menu = ['/', '/bio', '/blog', '/projects', '/contact']
   const brand = 'TonyRrr!'
+  const router = useRouter()
   const [showLinks, setShowLinks] = useState(false)
-  // Functions
 
+  /* Effects */
+  useEffect(() => {
+    //add event listener to router
+    router.events.on('routeChangeComplete', handleRouteChange)
+    //clean-up listener if component unmounts
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
+  /* Functions */
+  const handleRouteChange = () => {
+    setShowLinks(false)
+  }
   return (
     <>
       <Helm className={classes.helm} onClick={() => setShowLinks(!showLinks)} />
@@ -41,11 +54,11 @@ export default function Navi() {
         <ul className={classes.navLinks(showLinks)}>
           {menu.map((item, index) => (
             <li key={index}>
-              <Link href={item}>
-                <a className={classes.navLink(showLinks)}>
-                  {item != '/' ? item.slice(1) : 'home'}
-                </a>
-              </Link>
+              <a
+                onClick={() => router.push(item)}
+                className={classes.navLink(showLinks)}>
+                {item != '/' ? item.slice(1) : 'home'}
+              </a>
             </li>
           ))}
         </ul>
