a87ea6d291683766414ffe42dc124fb1f2006099
ActiveAvi <loch.hollows@gmail.com>
2021-05-31 10:12:06 -0400

    styled hash tag, fixed spacing

--- components/projects.js
+++ components/projects.js
@@ -3,11 +3,12 @@ import { Carousel } from 'react-responsive-carousel'
 import Fade from 'react-reveal/Fade'
 
 const classes = {
-  projectSection: `pb-20 sm:px-20 bg-warmgray-800 `,
-  projectHeader: 'px-2 py-14 text-2xl min-w-xs',
+  projectSection: `pb-20 py-14 sm:px-20 bg-warmgray-800 `,
+  projectHeader: 'px-2 text-3xl min-w-xs',
   project: 'py-28 px-10 sm:px-16',
   carousel: 'md:px-20',
   projectLink: 'text-yellow-100',
+  hash: 'text-green-400 font-extrabold text-2xl',
 }
 
 export default function Projects() {
@@ -17,54 +18,60 @@ export default function Projects() {
   return (
     <section className={classes.projectSection}>
       <Fade left>
-        <h1 className={classes.projectHeader}>#Projects</h1>
+        <h1 className={classes.projectHeader}>
+          <span className={classes.hash}>#</span>Projects
+        </h1>
       </Fade>
 
-      <Carousel className={classes.carousel} showThumbs={false}>
-        <div className={classes.project}>
-          <h3>
-            Project 1:{' '}
-            <a className={classes.projectLink} href='https://tonyrrr.dev'>
-              tonyrrr.dev
-            </a>
-          </h3>
-          <p>
-            This static site was created by me using the following tech-stack:
-          </p>
-          <ul>
-            <li>[frontend]: NextJs, Tailwindcss</li>
-            <li>[backend]: IPFS, NodeJs + Express for API</li>
-          </ul>
-          <p>
-            The site is hosted on IPFS, but also has a remote server for secured
-            endpoints!
-          </p>
-        </div>
-        <div className={classes.project}>
-          <h3>
-            Project 2:{' '}
-            <a className={classes.projectLink} href='https://getautonomy.info'>
-              getautonomy.info
-            </a>
-          </h3>
-          <p>
-            There isn't much I holder closer to my heart than Autonomy. Back in
-            2019 I: worked on the landing page and CRM automation.
-          </p>
-        </div>
-        <div className={classes.project}>
-          <h3>
-            Project 3:{' '}
-            <a className={classes.projectLink} href='https://badnarik.org/'>
-              badnarik.org
-            </a>
-          </h3>
-          <p>
-            Badnarik doesn't beat around the bush, that's why I like him. I got
-            the pleasure of setting up the ecommerce store for his site.
-          </p>
-        </div>
-      </Carousel>
+      <Fade bottom>
+        <Carousel className={classes.carousel} showThumbs={false}>
+          <div className={classes.project}>
+            <h3>
+              Project 1:{' '}
+              <a className={classes.projectLink} href='https://tonyrrr.dev'>
+                tonyrrr.dev
+              </a>
+            </h3>
+            <p>
+              This static site was created by me using the following tech-stack:
+            </p>
+            <ul>
+              <li>[frontend]: NextJs, Tailwindcss</li>
+              <li>[backend]: IPFS, NodeJs + Express for API</li>
+            </ul>
+            <p>
+              The site is hosted on IPFS, but also has a remote server for
+              secured endpoints!
+            </p>
+          </div>
+          <div className={classes.project}>
+            <h3>
+              Project 2:{' '}
+              <a
+                className={classes.projectLink}
+                href='https://getautonomy.info'>
+                getautonomy.info
+              </a>
+            </h3>
+            <p>
+              There isn't much I holder closer to my heart than Autonomy. Back
+              in 2019 I: worked on the landing page and CRM automation.
+            </p>
+          </div>
+          <div className={classes.project}>
+            <h3>
+              Project 3:{' '}
+              <a className={classes.projectLink} href='https://badnarik.org/'>
+                badnarik.org
+              </a>
+            </h3>
+            <p>
+              Badnarik doesn't beat around the bush, that's why I like him. I
+              got the pleasure of setting up the ecommerce store for his site.
+            </p>
+          </div>
+        </Carousel>
+      </Fade>
     </section>
   )
 }
