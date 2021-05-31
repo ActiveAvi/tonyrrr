a9f3f2279fdff0e786ef6a62fe745fb809c7cf3c
ActiveAvi <loch.hollows@gmail.com>
2021-05-31 09:54:06 -0400

    fixed styling issue

--- components/hero.js
+++ components/hero.js
@@ -3,8 +3,8 @@ import Link from 'next/link'
 
 const classes = {
   hero: `pt-36 bg-center bg-cover h-screen text-center`,
-  heroHeader: `text-lg sm:text-2xl font-medium text-gray-50 max-w-xs`,
-  heroText: `text-gray-50 mb-6 max-w-sm w-max`,
+  heroHeader: `text-lg msm:px-8 sm:text-2xl font-medium text-gray-50 max-w-xs`,
+  heroText: `text-gray-50 mb-6 max-w-sm msm:px-10`,
   heroInner: `grid grid-cols-1 content-center justify-items-center m-auto w-11/12 max-w-lg h-56  bg-gray-900 bg-opacity-40 min-h-40 rounded-full shadow-big`,
   heroButton: `w-max bg-blue-600 text-gray-50 rounded p-1 cursor-pointer transition duration-300 hover:bg-yellow-200 hover:text-gray-700`,
 }
