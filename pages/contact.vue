9ede0b99ba994ce8ab1e92a3a67e91d37a2c65ee
ActiveAvi <loch.hollows@gmail.com>
2021-07-06 06:23:23 -0400

    initial commit

---
+++ pages/contact.vue
@@ -0,0 +1,12 @@
+<template>
+  <contact-form />
+</template>
+
+<script>
+import ContactForm from '~/components/ContactForm.vue'
+export default {
+  components: { ContactForm },
+}
+</script>
+
+<style></style>
